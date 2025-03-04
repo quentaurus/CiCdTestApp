30e3a110b6db5074f771f0d66576c8dea1697062
Anders Averö <quentaurus@gmail.com>
2025-03-04 10:03:08 +0100

    Fixed bug

--- src/test/java/com/example/HelloTest.java
+++ src/test/java/com/example/HelloTest.java
@@ -8,6 +8,6 @@ class HelloTest {
 
 	@Test
 	void testHelloMessage() {
-		assertEquals("Hello, Java Desktop Application!!!", Hello.getHelloMessage());
+		assertEquals("Hello, Java Desktop Application!", Hello.getHelloMessage());
 	}
 }
