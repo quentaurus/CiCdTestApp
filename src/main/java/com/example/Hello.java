f53652e75cb35f46782e28b2146334a5d4f67ee8
Anders Averö <quentaurus@gmail.com>
2025-03-04 10:00:58 +0100

    Small updates

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class Main {
+public class Hello {
     public static void main(String[] args) {
         System.out.println(getHelloMessage());
     }
---
+++ src/test/java/com/example/HelloTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+
+	@Test
+	void testHelloMessage() {
+		assertEquals("Hello, Java Desktop Application!!!", Hello.getHelloMessage());
+	}
+}
